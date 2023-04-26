48b2340614d3a1c9e546fa3f17f08254b86f7814
root <yangzhipeng1108@163.com>
2023-04-26 17:00:39 +0800

    first commit

---
+++ run_test.py
@@ -0,0 +1,9 @@
+# /root/nas-share/chat/chatllama/facebook/opt-125m
+import pandas as pd
+
+'''
+python train.py --actor-model /root/nas-share/chat/chatllama/facebook/opt-125m --reward-model /root/nas-share/chat/chatllama/facebook/opt-125m --deployment-type single_gpu
+
+
+python train.py --actor-model THUDM/chatglm-6b --reward-model facebook/opt-350m --deployment-type single_gpu
+'''

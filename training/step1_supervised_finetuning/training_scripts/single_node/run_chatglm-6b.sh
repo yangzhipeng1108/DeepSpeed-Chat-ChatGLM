1afb36e5592804726b10559ebd96284efd9edca9
NLP <33448441+yangzhipeng1108@users.noreply.github.com>
2023-05-10 14:31:31 +0800

    Update run_chatglm-6b.sh

--- training/step1_supervised_finetuning/training_scripts/single_node/run_chatglm-6b.sh
+++ training/step1_supervised_finetuning/training_scripts/single_node/run_chatglm-6b.sh
@@ -3,10 +3,16 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # DeepSpeed Team
-
-# Note that usually LoRA needs to use larger learning rate
-OUTPUT_PATH=./output
-mkdir -p $OUTPUT_PATH
+OUTPUT=$1
+ZERO_STAGE=$2
+if [ "$OUTPUT" == "" ]; then
+    OUTPUT=./output
+fi
+if [ "$ZERO_STAGE" == "" ]; then
+    ZERO_STAGE=2
+fi
+mkdir -p $OUTPUT
+printf "Hello, Shell"
 
 deepspeed main.py \
    --data_path Smart/Q_A\
@@ -14,17 +20,14 @@ deepspeed main.py \
    --per_device_train_batch_size 8 \
    --per_device_eval_batch_size 8 \
    --max_seq_len 512 \
-   --learning_rate 1e-3 \
-   --weight_decay 0.1 \
+   --learning_rate 9.65e-6 \
+   --weight_decay 0. \
    --num_train_epochs 16 \
    --gradient_accumulation_steps 1 \
    --lr_scheduler_type cosine \
    --num_warmup_steps 0 \
    --seed 1234 \
-   --zero_stage 0 \
-   --lora_dim 128 \
-   --lora_module_name decoder.layers. \
-   --only_optimize_lora \
+   --zero_stage $ZERO_STAGE \
    --deepspeed \
-   --output_dir $OUTPUT_PATH \
-   &> $OUTPUT_PATH/training.log
+   --output_dir $OUTPUT \
+   &> $OUTPUT/training.log

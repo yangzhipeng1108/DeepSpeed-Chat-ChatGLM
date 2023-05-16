eea40ca6bd295384c8cb7caf17f1d5b433a3b704
NLP <33448441+yangzhipeng1108@users.noreply.github.com>
2023-05-16 08:45:08 +0800

    Update inference.sh

--- inference.sh
+++ inference.sh
@@ -1,1 +1,1 @@
-python chat.py --path   python chat.py --path  output/step3-models/chatglm-6b/actor #{ATH-to-your-actor-model}
+python chat.py --path   python chat.py --path  output/step3-models/chatglm-6b/actor #{PATH-to-your-actor-model}
